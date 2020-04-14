57f04f3ac38222a29190411be6e03cfab3b8901b
Tamas Simon <tom_76@t-online.hu>
2020-04-14 08:19:57 +0200

    Delete old bulets, limit bullet numbers

--- alien_invasion.py
+++ alien_invasion.py
@@ -30,8 +30,18 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
-            self._update_screen()   
+            self._update_bullets()
+            self._update_screen()
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Upadate bullet positions
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)   
 
     def _check_events(self):
         # Watch for keyboard and mouse events.
@@ -65,17 +75,16 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bulet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def _update_screen(self):
         # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
             for bullet in self.bullets.sprites():
-                bullet.draw_bullet(
-                    
-                )
+                bullet.draw_bullet()
             # Make the most recently drawn screen visible.
             pygame.display.flip()
 
