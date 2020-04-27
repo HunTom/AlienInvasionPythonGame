3a062c1734c28b01e3c6ec50a1bf7ef642af7e3d
Tamas Simon <tom_76@t-online.hu>
2020-04-27 20:59:02 +0200

    shot down aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -46,7 +46,20 @@ class AlienInvasion:
         # Get rid of bullets that have disappeared.
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
-                self.bullets.remove(bullet)   
+                self.bullets.remove(bullet)  
+
+        self._check_bullet_alien_collision()
+
+    def _check_bullet_alien_collision(self):
+        """Respond to bullet-alien collisions."""
+        # Check for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True) 
+
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
 
     def _update_aliens(self):
         """check if the fleet is at an edge
